849874b41a0a420a1d686dbaca9504967f446bb1
Imran <imranmansuri038@gmail.com>
2023-10-26 18:21:37 +0530

    api url added

--- frontend/src/store/slice/productsApi.js
+++ frontend/src/store/slice/productsApi.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shoes-api-bsxr.onrender.com/",
+  }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "products",
